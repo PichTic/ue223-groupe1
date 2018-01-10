88919d0c769f0b03d494026a1abd3d262f0784f8
VinzPich <pichot.vincent@gmail.com>
2018-01-10 17:59:23 +0100

    fixed elseif

--- search_Trad_request.php
+++ search_Trad_request.php
@@ -1,26 +1,23 @@
 <?php
-	require_once './require.php';
-	
-	//Déclaration des variables
-	$mot = strip_tags($_POST['mot']);
-	$langueOrigine = strip_tags($_POST['langueOrigine']);
-	$langueCible = strip_tags($_POST['langueCible']);
-	
-	$connect = connect($db);
-	
-	//Requête pour chercher dans la base le mot traduit en fonction de la langue d'origine
-	if($langueOrigine = 'FR'){
-		$requete = $connect->prepare('SELECT * FROM mots WHERE FR = :mot');
-	}
-	else if($langueOrigine = 'EN'){
-		$requete = $connect->prepare('SELECT * FROM mots WHERE EN = :mot');
-	}
-	else{
-		$requete = $connect->prepare('SELECT * FROM mots WHERE ES = :mot');
-	}
-	$requete->execute([ ':mot' => $mot, ]);
-	$resultat = $requete->fetch();
-	
-	//On renvoit le résultat du mot traduit
-	echo $resultat[$langueCible];
-?>
+    require_once './require.php';
+
+    //Déclaration des variables
+    $mot = strip_tags($_POST['mot']);
+    $langueOrigine = strip_tags($_POST['langueOrigine']);
+    $langueCible = strip_tags($_POST['langueCible']);
+
+    $connect = connect($db);
+
+    //Requête pour chercher dans la base le mot traduit en fonction de la langue d'origine
+    if ($langueOrigine = 'FR') {
+        $requete = $connect->prepare('SELECT * FROM mots WHERE FR = :mot');
+    } elseif ($langueOrigine = 'EN') {
+        $requete = $connect->prepare('SELECT * FROM mots WHERE EN = :mot');
+    } else {
+        $requete = $connect->prepare('SELECT * FROM mots WHERE ES = :mot');
+    }
+    $requete->execute([':mot' => $mot]);
+    $resultat = $requete->fetch();
+
+    //On renvoit le résultat du mot traduit
+    echo $resultat[$langueCible];
